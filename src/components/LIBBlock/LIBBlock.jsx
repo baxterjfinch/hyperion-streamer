80acb0653dc702eaa3befe431ac03a32a29d68dd
baxter <cryptohemlock@protonmail.com>
2020-08-07 17:56:27 -0700

    lib block test updates

--- src/components/LIBBlock/LIBBlock.jsx
+++ src/components/LIBBlock/LIBBlock.jsx
@@ -3,20 +3,31 @@ import { connect } from "react-redux";
 
 const LIBBlock = (props) => {
   const [block, setBlock] = useState(null);
-
+  const [fork, setFork] = useState(null);
   useEffect(()=>{
 
   }, [])
 
   props.client.onLIB = async (data) => {
     if (data.block_num % 5 === 0) {
-      setBlock(data.block_num)
+      // setBlock(data.block_num)
+      console.log("LIB ", data.block_num)
+
     }
+  }
+  props.client.onFork = async (data) => {
+    console.log("FORK ", data)
+
+  }
+
+  props.client.onData = async (data) => {
+    console.log("DATA ", data)
   }
 
   return (
     <div className="">
       Block: {block}
+      Fork: {fork}
   	</div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -12,28 +12,9 @@ import App from 'app/index';
 
 const HyperionSocketClient = require('@eosrio/hyperion-stream-client').default;
 const client = new HyperionSocketClient('http://127.0.0.1:7770', {async: false});
-
-
-
-
-// client.connect(() => {
-//
-// })
-
-// client.onLIB = async (data) => {
-//   console.log(data)
-// }
-
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-//   document.getElementById('root')
-// );
-
 client.onConnect = (data) => {
   console.log("connected")
-
+  console.log(process.env.ES_PASSWORD)
   // client.disconnect();
 }
 
@@ -57,7 +38,3 @@ if (rootElement.hasChildNodes()) {
 } else {
   render(AppBundle, rootElement);
 }
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-// serviceWorker.unregister();
